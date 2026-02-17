affe34e0198a083b37153ad440db1e9bb58f12af
Viral <viralsir2018@gmail.com>
2026-02-17 08:52:17 +0530

    function overloading has been introduced.

--- src/oop/FunctionOverLoadingDemo.java
+++ src/oop/FunctionOverLoadingDemo.java
@@ -2,5 +2,24 @@ package oop;
 
 public class FunctionOverLoadingDemo
 {
+    static int  total(int no1,int no2)
+    {
+        return no1+no2;
+    }
+    static int total(int no1,int no2,int no3)
+    {
+        return no1+no2+no3;
+    }
+//    static void total(int no1,int no2)
+//    {
+//        System.out.println(no1+no2);
+//    }
+
+    public static void main(String[] args)
+    {
+
+        System.out.println(total(12,23));
+        System.out.println(total(23,23,22));
+    }
 
 }
